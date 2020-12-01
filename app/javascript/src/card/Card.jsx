9a0d39b856909275c49d3540c257a5dc5fb31086
matt_kuras <mattkuras@gmail.com>
2020-12-01 16:42:41 -0500

    overlap cards

--- app/javascript/src/card/Card.jsx
+++ app/javascript/src/card/Card.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 // import TinderCard from "react-tinder-card";
 // import {FaRegCheckCircle} from 'react-icons/fa'
-import './Card.css'
+import "./Card.css";
 
 const Card = (props) => {
-
-
-
   return (
-    // <TinderCard>
-      <div className='tinder-card'>
-          <span>{props.name}</span>
-          {/* <FaRegCheckCircle/> */}
+    <div className='card-container'>
+      <div className="tinder-card">
+        <span>{props.name}</span>
+        {/* <FaRegCheckCircle/> */}
       </div>
-    // </TinderCard>
+    </div>
   );
 };
 
